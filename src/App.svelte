faff01dafc28f93020f33e651bf4657ef448f342
BuildTools <cephas8080@outlook.com>
2022-01-29 10:05:57 -0100

    the foundation of the calculator

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,43 @@
 <script>
-	export let name;
+    let row1 = [1,2,3];
+    let row2 = [4,5,6];
+    let row3 = [7,8,9];
+    function onclicked(test){
+        console.log(test)
+    }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+.screen{
+    resize: none;
+    font-size: 10vw;
+    width: 70vw;
+}
+button{
+    height: calc(70vw/3);
+    width: calc(70vw/3);
+    font-size: 15vw;
+}
 </style>
+<main>
+    <input type="textfield" class="screen" name="screen">
+    <br>
+    <table>
+        <tr>
+            {#each row3 as rows3}
+                <td><button on:click={() => onclicked(rows3)} >{rows3}</button></td>
+
+            {/each}
+        </tr>
+        <tr>
+            {#each row2 as rows2}
+                <td><button on:click={() => onclicked(rows2)} >{rows2}</button></td>
+            {/each}
+        </tr>
+        <tr>
+            {#each row1 as rows1}
+                <td><button on:click={() => onclicked(rows1)} >{rows1}</button></td>
+            {/each}
+        </tr>
+    </table>
+</main>
