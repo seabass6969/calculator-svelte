826cb1ca76dec688ac5d6cf8fb66a16127ab170d
BuildTools <cephas8080@outlook.com>
2022-02-04 20:54:07 -0100

    offline seems to be working

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,38 @@
 console.log("service worker loaded")
-let deferredPrompt;
+const allcontent = [
+    "/offline.html",
+    "/sw.js",
+    "/build/bundle.js",
+    "/build/bundle.css",
+    "/logo.svg",
+    "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&family=Space+Mono&display=swap",
+    "/index.html",
+    "favicon.png",
+    "icon_144x144.png",
+    "icon_168x168.png",
+    "icon_192x192.png",
+    "icon_48x48.png",
+    "icon_512x512.png",
+    "icon_72x72.png",
+    "icon_96x96.png",
+]
+let cache_name = "svelte-calculator-do-not-click-me";
+self.addEventListener("install",Event=> {
+    console.log("adding me into cache");
+    Event.waitUntil(
+        caches.open(cache_name).then(cache => {return cache.addAll(allcontent);}).catch(err => console.log(err))
+    )
+})
 
-if (window !== undefined){
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cache_name);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
-}
-
